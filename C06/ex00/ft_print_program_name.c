0a41262d8aaa2cd05445171c2d66792df093493d
Civan Rumet Arı <civanrumet@hotmail.com>
2024-02-14 04:20:56 +0300

    fixes

--- C06/ex00/ft_print_program_name.c
+++ C06/ex00/ft_print_program_name.c
@@ -6,7 +6,7 @@
 /*   By: cari <cari@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 16:42:20 by cari              #+#    #+#             */
-/*   Updated: 2024/02/13 22:24:24 by cari             ###   ########.fr       */
+/*   Updated: 2024/02/14 04:20:29 by cari             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ unsigned int	ft_strlen(char *str);
 
 int	main(int argc, char **argv)
 {
-
 	(void) argc;
 	write(1, argv[0], ft_strlen(argv[0]));
 	write(1, "\n", 1);
