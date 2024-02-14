aa940ba4b593c05f5536a964bfa36ce74616bb8c
Civan Rumet Arı <civanrumet@hotmail.com>
2024-02-15 01:16:18 +0300

    fixes

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -6,28 +6,9 @@
 /*   By: cari <cari@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 04:05:24 by cari              #+#    #+#             */
-/*   Updated: 2024/02/14 17:29:14 by cari             ###   ########.fr       */
+/*   Updated: 2024/02/15 00:05:46 by cari             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	total(int size, char **strs);
-
-void	printer(int size, char **strs, char *sep, char *ptr);
-
-char	*ft_strjoin(int size, char **strs, char *sep)
-{
-	char *ptr;
-	int	length_strs;
-	int length_sep;
-
-	length_strs = total(size, strs);
-	length_sep = 0;
-	while (sep[length_sep])
-		length_sep++;
-	ptr = (char *)malloc(length_strs + (size -1)*length_sep +1);
-	printer(size, strs, sep, ptr);
-	return (ptr);
-}
 
 int	total(int size, char **strs)
 {
@@ -47,9 +28,9 @@ int	total(int size, char **strs)
 
 void	printer(int size, char **strs, char *sep, char *ptr)
 {
-	int counter;
-	int i;
-	int counter2;
+	int	counter;
+	int	i;
+	int	counter2;
 
 	counter = 0;
 	counter2 = 0;
@@ -57,22 +38,27 @@ void	printer(int size, char **strs, char *sep, char *ptr)
 	while (counter < size)
 	{
 		while (strs[counter][i])
-		{
-			ptr[counter2] = strs[counter][i];
-			i++;
-			counter2++;
-		}
+			ptr[counter2++] = strs[counter][i++];
 		i = 0;
 		counter++;
-		if (counter != size)
-		{
-			while (sep[i])
-			{
-				ptr[counter2] = sep[i];
-				i++;
-				counter2++;
-			}
-		}
+		while (sep[i] && counter != size)
+			ptr[counter2++] = sep[i++];
 		i = 0;
 	}
+	ptr[counter2] = '\0';
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*ptr;
+	int		length_strs;
+	int		length_sep;
+
+	length_strs = total(size, strs);
+	length_sep = 0;
+	while (sep[length_sep])
+		length_sep++;
+	ptr = (char *)malloc(length_strs + (size -1) * length_sep +1);
+	printer(size, strs, sep, ptr);
+	return (ptr);
 }
